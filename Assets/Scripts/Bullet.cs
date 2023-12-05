46496b9c4977725ef7d1724cee721474ddc9aad6
Thomas-Chanu <102727315+Thomas-Chanu@users.noreply.github.com>
2023-12-05 18:15:06 +0100

    Ajout de knockback + correction des particules mal affichées

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float knockBackForce;
+
     public GameObject hurtParticlePrefab;
     public GameObject impactParticlePrefab;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
             if (collision.GetComponent<Enemy>().hp > 1 ) Instantiate(hurtParticlePrefab, transform.position, transform.rotation);
+            Vector3 vectEnnemyToBullet = (collision.transform.position - transform.position).normalized;
+            collision.GetComponent<Rigidbody2D>().AddForce((vectEnnemyToBullet + transform.right).normalized * knockBackForce);
             collision.GetComponent<Enemy>().TakeDamage(1);
             popParticles();
         }
