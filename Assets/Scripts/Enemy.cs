0f59a3231c4eb8f707fef71c0cbd9f4f9460da3d
Thoringar <guillaumedelboulbes@gmail.com>
2023-12-06 01:13:23 +0100

    Rajout d'une pièce 3D pour le test

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,7 +9,7 @@ public class Enemy : MonoBehaviour
 
     public GameObject ParticlePrefab;
 
-    public GameObject moneyPrefab;
+    public List<GameObject> moneyPrefabs;
 
     private void Start()
     {
@@ -41,9 +41,9 @@ public class Enemy : MonoBehaviour
         Instantiate(ParticlePrefab, transform.position, Quaternion.identity);
 
         GameObject currentBill;
-        for(int i = 0; i < Random.Range(1, 5); i++) 
+        for(int i = 0; i < Random.Range(1, 8); i++) 
         { 
-            currentBill = Instantiate(moneyPrefab, transform.position, Quaternion.identity);
+            currentBill = Instantiate(moneyPrefabs[Random.Range(0,2)], transform.position, Quaternion.identity);
 
             currentBill.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2f , 2f), Random.Range(-2f, 2f), -3f), ForceMode.Impulse);
             currentBill.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10, 10)), ForceMode.Impulse);
